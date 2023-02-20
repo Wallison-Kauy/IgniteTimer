d25eae34ae4c4ebfd1984c4d5ead82c7907c25c2
Wallison Kauy <wallisonkauy@gmail.com>
2023-02-20 11:19:58 -0300

    ajustando para aparecer o titulo

--- src/pages/Home/NewCycleForm/index.tsx
+++ src/pages/Home/NewCycleForm/index.tsx
@@ -12,35 +12,43 @@ export function NewCycleForm() {
 
   return (
     <FormContainer>
-      <label htmlFor="task">Vou trabalhar em</label>
-      <TaskInput
+
+    {activeCycle ? (
+      <h1> {activeCycle?.task}</h1> 
+    ) 
+    : (
+      <>
+        <label htmlFor="task">Vou trabalhar em</label>
+        <TaskInput
         id="task"
         list="task-suggetions"
         placeholder="De um nome para o seu projeto"
         disabled={!!activeCycle}
         {...register("task")}
-      />
+        />
 
-      <datalist id="task-suggetions">
-        <option value="Projeto 1"></option>
-        <option value="Projeto 2"></option>
-        <option value="Projeto 3"></option>
-        <option value="Banana"></option>
-      </datalist>
+        <datalist id="task-suggetions">
+          <option value="Projeto 1"></option>
+          <option value="Projeto 2"></option>
+          <option value="Projeto 3"></option>
+          <option value="Banana"></option>
+        </datalist>
 
-      <label htmlFor="minutsAmount">Durante</label>
-      <MinutsAmoutInput
-        id="minutsAmount"
-        type="number"
-        placeholder="00"
-        step={5}
-        min={5}
-        max={60}
-        disabled={!!activeCycle}
-        {...register("minutesAmount", { valueAsNumber: true })}
-      />
-
+        <label htmlFor="minutsAmount">Durante</label>
+        <MinutsAmoutInput
+          id="minutsAmount"
+          type="number"
+          placeholder="00"
+          step={5}
+          min={5}
+          max={60}
+          disabled={!!activeCycle}
+          {...register("minutesAmount", { valueAsNumber: true }
+        )}
+        />
       <span>minutos.</span>
+      </>
+    )}
     </FormContainer>
   );
 }
